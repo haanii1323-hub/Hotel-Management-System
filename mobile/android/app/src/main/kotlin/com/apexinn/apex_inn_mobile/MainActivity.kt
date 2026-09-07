e148c719f0032d8d483d5ab117e5dc7d4a575a09
Hani <admin@apexinn.com>
2026-09-08 01:05:08 +0530

    chore: remove mobile folder and keep web-first architecture

--- mobile/android/app/src/main/kotlin/com/apexinn/apex_inn_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.apexinn.apex_inn_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:apex_inn_mobile/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ApexInnApp());
-    expect(find.text('Apex INN PMS'), findsOneWidget);
-  });
-}
